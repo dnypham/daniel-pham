65f56a90e2b996062098c4567ef960bb80b073ce
Daniel Pham <dnypham@gmail.com>
2022-03-13 19:05:30 +0000

    Create Home component. Add photo and text.

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from './components/navigation';
+import Home from './components/home';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Navigation />
+      <>
+        <Navigation />
+        <Home />
+      </>
     );
   }
 }
