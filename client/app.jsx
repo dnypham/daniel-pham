3a96e47bedea1d042eae7679490f67163fe5ae29
Daniel Pham <dnypham@gmail.com>
2022-03-16 23:02:34 +0000

    Add functionality to close modal when selecting a section.

--- client/app.jsx
+++ client/app.jsx
@@ -12,7 +12,7 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      isActive: ''
+      isActive: null
     };
 
     this.home = React.createRef();
--- client/components/navigation-modal.jsx
+++ client/components/navigation-modal.jsx
@@ -8,12 +8,12 @@ export default class NavigationModal extends React.Component {
           <i className="fa-solid fa-xmark fa-xl" onClick={this.props.closeNavigationModal}></i>
         </div>
         <ul className='navigation-list-modal nunito-900'>
-          <li><a className={this.props.isActive === 'home' ? 'active' : 'inactive'} href='#home'>HOME</a></li>
-          <li><a className={this.props.isActive === 'about' ? 'active' : 'inactive'} href='#about'>ABOUT</a></li>
-          <li><a className={this.props.isActive === 'skills' ? 'active' : 'inactive'} href='#skills'>SKILLS</a></li>
-          <li><a className={this.props.isActive === 'tools' ? 'active' : 'inactive'} href='#tools'>TOOLS</a></li>
-          <li><a className={this.props.isActive === 'projects' ? 'active' : 'inactive'} href='#projects'>PROJECTS</a></li>
-          <li><a className={this.props.isActive === 'contact' ? 'active' : 'inactive'} href='#contact'>CONTACT</a></li>
+          <li><a className={this.props.isActive === 'home' ? 'active' : 'inactive'} href='#home' onClick={this.props.closeNavigationModal}>HOME</a></li>
+          <li><a className={this.props.isActive === 'about' ? 'active' : 'inactive'} href='#about' onClick={this.props.closeNavigationModal}>ABOUT</a></li>
+          <li><a className={this.props.isActive === 'skills' ? 'active' : 'inactive'} href='#skills' onClick={this.props.closeNavigationModal}>SKILLS</a></li>
+          <li><a className={this.props.isActive === 'tools' ? 'active' : 'inactive'} href='#tools' onClick={this.props.closeNavigationModal}>TOOLS</a></li>
+          <li><a className={this.props.isActive === 'projects' ? 'active' : 'inactive'} href='#projects' onClick={this.props.closeNavigationModal}>PROJECTS</a></li>
+          <li><a className={this.props.isActive === 'contact' ? 'active' : 'inactive'} href='#contact' onClick={this.props.closeNavigationModal}>CONTACT</a></li>
         </ul>
       </div>
     );
