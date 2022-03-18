56192b98ab42a151b970df06998ab05c4a562007
Daniel Pham <dnypham@gmail.com>
2022-03-18 23:09:59 +0000

    Refactor navigation bar to share functions with navigation modal.

--- client/components/navigation-modal.jsx
+++ client/components/navigation-modal.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
 export default class NavigationModal extends React.Component {
-  checkAbout() {
-    const aboutArray = ['about', 'textOne', 'textTwo', 'textThree'];
-
-    if (aboutArray.includes(this.props.isActive)) {
-      return 'active';
-    } else {
-      return 'inactive';
-    }
-  }
-
   render() {
     return (
       <div className={`navigation-modal ${this.props.isNavigationOpen ? 'view' : ''}`}>
@@ -19,7 +9,7 @@ export default class NavigationModal extends React.Component {
         </div>
         <ul className='navigation-list-modal nunito-900'>
           <li><a className={this.props.isActive === 'home' ? 'active' : 'inactive'} href='#home' onClick={this.props.closeNavigationModal}>HOME</a></li>
-          <li><a className={this.checkAbout} href='#about' onClick={this.props.closeNavigationModal}>ABOUT</a></li>
+          <li><a className={this.props.checkAbout()} href='#about' onClick={this.props.closeNavigationModal}>ABOUT</a></li>
           <li><a className={this.props.isActive === 'skills' ? 'active' : 'inactive'} href='#skills' onClick={this.props.closeNavigationModal}>SKILLS</a></li>
           <li><a className={this.props.isActive === 'tools' ? 'active' : 'inactive'} href='#tools' onClick={this.props.closeNavigationModal}>TOOLS</a></li>
           <li><a className={this.props.isActive === 'projects' || this.props.isActive === 'event' || this.props.isActive === 'brew' ? 'active' : 'inactive'} href='#projects' onClick={this.props.closeNavigationModal}>PROJECTS</a></li>
--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -11,6 +11,7 @@ export default class Navigation extends React.Component {
 
     this.openNavigationModal = this.openNavigationModal.bind(this);
     this.closeNavigationModal = this.closeNavigationModal.bind(this);
+    this.checkAbout = this.checkAbout.bind(this);
   }
 
   openNavigationModal() {
@@ -23,15 +24,30 @@ export default class Navigation extends React.Component {
     document.body.classList.remove('no-scroll');
   }
 
+  checkAbout() {
+    const aboutArray = ['about', 'textOne', 'textTwo', 'textThree'];
+
+    if (aboutArray.includes(this.props.isActive)) {
+      return 'active';
+    } else {
+      return 'inactive';
+    }
+  }
+
   render() {
     return (
       <div className='navigation-container space-between fixed'>
-        <NavigationModal closeNavigationModal={this.closeNavigationModal} isNavigationOpen={this.state.isNavigationOpen} isActive={this.props.isActive}/>
+        <NavigationModal
+          closeNavigationModal={this.closeNavigationModal}
+          isNavigationOpen={this.state.isNavigationOpen}
+          isActive={this.props.isActive}
+          checkAbout={this.checkAbout}
+        />
         <h1 className='nunito-900'>DP</h1>
         <i className="fa-solid fa-bars fa-lg" onClick={this.openNavigationModal}></i>
         <ul className='navigation-list nunito-900'>
           <li><a className={this.props.isActive === 'home' ? 'active' : 'inactive'} href='#home'>HOME</a></li>
-          <li><a className={this.props.isActive === 'about' ? 'active' : 'inactive'} href='#about'>ABOUT</a></li>
+          <li><a className={this.checkAbout()} href='#about'>ABOUT</a></li>
           <li><a className={this.props.isActive === 'skills' ? 'active' : 'inactive'} href='#skills'>SKILLS</a></li>
           <li><a className={this.props.isActive === 'tools' ? 'active' : 'inactive'} href='#tools'>TOOLS</a></li>
           <li><a className={this.props.isActive === 'projects' || this.props.isActive === 'event' || this.props.isActive === 'brew' ? 'active' : 'inactive'} href='#projects'>PROJECTS</a></li>
