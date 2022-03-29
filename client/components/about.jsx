b0d61d8e37920f6332f1f7283c1a99067b068640
Daniel Pham <dnypham@gmail.com>
2022-03-29 23:13:48 +0000

    Add updated About Me.

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -8,23 +8,23 @@ export default class About extends React.Component {
         <div className='about-text-container varela-round fade-in from-bottom' ref={this.props.aboutSectionRefs.content}>
           <div className='about-content-container'>
             <p>
-              Hello! My name is Daniel Pham and I&apos;m a Software Developer.
+              My name is Daniel Pham and I&apos;m a Full Stack Web Developer currently located in Orange County, CA.
             </p>
             <p>
-              My interest in coding began when I started an online Web Development course called The Odin Project.
-              During the course, my first project was to recreate the Google homepage
-              using only HTML and CSS. Completing this project was the moment that I realized
-              I wanted to pursue programming as my career. I was addicted.
+              I discovered programming through an online course called The Odin Project.
+              My first project in the course was to recreate the Google Homepage and upon completing it,
+              I was instantly addicted to the process. The Odin Project really piqued my interest in pursuing
+              programming as a career. Soon after, I had an opportunity to join an Accelerated Web Development
+              Program at LearningFuze and jumped on the opportunity to pursue my newfound passion.
             </p>
             <p>
-              Soon after, I had the opportunity to join a Web Development Bootcamp at LearningFuze.
-              I put my life on hold and took the risk and it was the best decision I
-              have made in my life. In such a short time, programming has become a passion of mine.
+              My time at LearningFuze cemented that programming will be my career path.
+              I have gained knowledge and experience building full stack web applications using HTML,
+              CSS, JavaScript, React, Node.js, Express, and PostgreSQL.
             </p>
             <p>
-              Recently, I graduated from LearningFuze and have started looking for my first position
-              as a Software Developer. I&apos;m hoping to find a company that shares the
-              same mindset and values as I do and that will set me on the right trajactory as I begin my career.
+              I am now in the job market and I&apos;m excited to work for and grow with a company
+              that shares the same mindset and values as I do.
             </p>
           </div>
           <div className='about-button-container'>
