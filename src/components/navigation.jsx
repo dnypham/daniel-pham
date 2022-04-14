de2e6e34135b62327d7b3b3fa21b45dfe01f4dcf
Daniel Pham <dnypham@gmail.com>
2022-04-14 15:06:33 -0700

    Refactor navigation.jsx to functional component.

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,71 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavigationModal from './navigation-modal';
 
-export default class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Navigation(props) {
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
 
-    this.state = {
-      isNavigationOpen: false
-    };
-
-    this.openNavigationModal = this.openNavigationModal.bind(this);
-    this.closeNavigationModal = this.closeNavigationModal.bind(this);
-    this.checkAbout = this.checkAbout.bind(this);
-    this.checkProjects = this.checkProjects.bind(this);
-  }
-
-  openNavigationModal() {
-    this.setState({ isNavigationOpen: true });
+  function openNavigationModal() {
+    setIsNavigationOpen(true);
     document.body.classList.add('no-scroll');
   }
 
-  closeNavigationModal() {
-    this.setState({ isNavigationOpen: false });
+  function closeNavigationModal() {
+    setIsNavigationOpen(false);
     document.body.classList.remove('no-scroll');
   }
 
-  checkAbout() {
+  function checkAbout() {
     const aboutIds = ['about', 'textOne', 'textTwo', 'textThree'];
 
-    if (aboutIds.includes(this.props.isSectionActive)) {
+    if (aboutIds.includes(props.isSectionActive)) {
       return 'active';
     } else {
       return 'inactive';
     }
   }
 
-  checkProjects() {
+  function checkProjects() {
     const projectsIds = ['projects', 'event', 'brew'];
 
-    if (projectsIds.includes(this.props.isSectionActive)) {
+    if (projectsIds.includes(props.isSectionActive)) {
       return 'active';
     } else {
       return 'inactive';
     }
   }
 
-  render() {
-    return (
-      <div className='navigation-container space-between fixed'>
-        <NavigationModal
-          closeNavigationModal={this.closeNavigationModal}
-          isNavigationOpen={this.state.isNavigationOpen}
-          isSectionActive={this.props.isSectionActive}
-          checkAbout={this.checkAbout}
-          checkProjects={this.checkProjects}
-        />
-        <h1 className='nunito-900'>DP</h1>
-        <i className="fa-solid fa-bars fa-lg" onClick={this.openNavigationModal}></i>
-        <ul className='navigation-list nunito-900'>
-          <li><a className={this.props.isSectionActive === 'home' ? 'active' : 'inactive'} href='#home'>HOME</a></li>
-          <li><a className={this.checkAbout()} href='#about'>ABOUT</a></li>
-          <li><a className={this.props.isSectionActive === 'skills' ? 'active' : 'inactive'} href='#skills'>SKILLS</a></li>
-          <li><a className={this.props.isSectionActive === 'tools' ? 'active' : 'inactive'} href='#tools'>TOOLS</a></li>
-          <li><a className={this.checkProjects()} href='#projects'>PROJECTS</a></li>
-          <li><a className={this.props.isSectionActive === 'contact' ? 'active' : 'inactive'} href='#contact'>CONTACT</a></li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className='navigation-container space-between fixed'>
+      <NavigationModal
+        closeNavigationModal={closeNavigationModal}
+        isNavigationOpen={isNavigationOpen}
+        isSectionActive={props.isSectionActive}
+        checkAbout={checkAbout}
+        checkProjects={checkProjects}
+      />
+      <h1 className='nunito-900'>DP</h1>
+      <i className="fa-solid fa-bars fa-lg" onClick={openNavigationModal}></i>
+      <ul className='navigation-list nunito-900'>
+        <li><a className={props.isSectionActive === 'home' ? 'active' : 'inactive'} href='#home'>HOME</a></li>
+        <li><a className={checkAbout()} href='#about'>ABOUT</a></li>
+        <li><a className={props.isSectionActive === 'skills' ? 'active' : 'inactive'} href='#skills'>SKILLS</a></li>
+        <li><a className={props.isSectionActive === 'tools' ? 'active' : 'inactive'} href='#tools'>TOOLS</a></li>
+        <li><a className={checkProjects()} href='#projects'>PROJECTS</a></li>
+        <li><a className={props.isSectionActive === 'contact' ? 'active' : 'inactive'} href='#contact'>CONTACT</a></li>
+      </ul>
+    </div>
+  );
 }
